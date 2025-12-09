58ff55a98a0939253d39a52a0c2c0f6ac69ea803
Keenan DePaz <kdepaz@san.rr.com>
2025-12-08 20:49:51 -0800

    Created a test file for Keenan called the BattleManagerTest.java.
    Tests the damage calculation of an ability and applies it to an enemy. Checks if the enemy took proper damage.

---
+++ BattleSystem/src/test/java/com/example/cs210battlemocks/BattleManagerTest.java
@@ -0,0 +1,55 @@
+package com.example.cs210battlemocks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//test case: when a playerCharacter attacks an enemy with an ability, the enemy will take the
+//specific amount of damage and return if the damage and math logic was correct or not.
+
+class BattleManagerTest {
+
+    private PlayerCharacter hero;
+    private Enemy slime;
+    private BattleManager manager;
+
+    // This runs before EVERY single @Test method to reset the state
+    @BeforeEach
+    void setUp() {
+        //set up our hero and our enemy
+        Stats heroStats = new Stats(15, 5, 10, 5); // Spd 10
+        Stats slimeStats = new Stats(10, 2, 5, 0); // Spd 5
+
+        hero = new PlayerCharacter("Test Hero", 100, 50, heroStats);
+        // We use SimpleAI since we know it works
+        slime = new Enemy("Test Slime", 50, 0, slimeStats, new SimpleAI());
+
+        //add them to the list to prepare for battle
+        List<PlayerCharacter> players = new ArrayList<>();
+        players.add(hero);
+        List<Enemy> enemies = new ArrayList<>();
+        enemies.add(slime);
+
+        //add them to the battle Manager to prepare for battle
+        manager = new BattleManager(players, enemies);
+    }
+
+    @Test
+    void testDamageCalculation() {
+        // Setup a fireball: 20 Power
+        // Dmg = Power(20) + Mag(5) - Def(2) = 23
+        Ability fireball = new Ability("Fireball", "Fire", 10, 20, null);
+
+        int initialHP = slime.getCurrentHP();
+        //have the hero shoot the fireball at the slime
+        fireball.execute(hero, slime);
+
+        //check if the damage equals.
+        assertEquals(initialHP - 23, slime.getCurrentHP(),
+                "Damage math should be exact (20+5-2=23).");
+    }
+}
